043e64816b274e61df32292c6cd073dbfed899f3
Jovan Dandridge <quantumcoder01@gmail.com>
2019-03-06 17:30:22 -0800

    Added .env file

--- movies/urls.py
+++ movies/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.home_page, name='home_page'),
+    path('', views.home_page, name='home_page'),
     # path('create/', views.create, name='create'),
     # path('edit/<str:movie_id>', views.edit, name='edit'),
     # path('delete/<str:movie_id>', views.delete, name='delete')
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from airtable import Airtable
+import os
 
-# Create your views here.
+
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+             'Movies',
+             api_key=os.environ.get('AIRTABLE_API_KEY')
+             )
+
+def home_page(request):
+    user_query = str(request.GET.get('query', ''))
+    return render(request, 'movies/movies_stuff.html')
